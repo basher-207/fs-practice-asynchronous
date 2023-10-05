e52be9f5cc06e29001c1241bf87a41a9bdd72de6
Vladyslav Diachenko <basher207vd@gmail.com>
2023-10-05 17:15:19 +0300

    Added functionality of summing numbers from files. Test01

--- app.js
+++ app.js
@@ -3,3 +3,51 @@ const path = require("path");
 const filesDir = "./files";
 
 //Write your code here, use readdir and readFile functions with callbacks.
+
+function asyncRead (path){
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        });
+    });
+};
+
+
+fs.readdir(filesDir, 'utf8', (err, files) => {
+    if(err) { 
+        console.log("No numbers for calculating");
+        return;
+    }
+    const promisesArr = [];
+    files.forEach((fName) =>{
+        const fPath = filesDir + "/" + fName;
+        promisesArr.push(asyncRead(fPath));
+    });
+
+    Promise.all(promisesArr)
+    .then((values) => {
+        const data = String(values);
+        const numbers = data.match(/\d+/g);
+        const sum = numbers.reduce((acc, curr) => {
+            return acc + Number(curr);
+        }, 0);
+        console.log(`The sum of the numbers in the files: ${sum}`);
+    }).catch(() => {
+        console.log("No numbers for calculating");
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
